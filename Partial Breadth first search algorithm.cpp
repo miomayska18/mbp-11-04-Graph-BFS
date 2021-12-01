3be701f54a95f4503ce455a209d7674b13a3e5b8
Magdalena <miomayska18@codingburgas.bg>
2021-12-01 09:19:06 +0200

    complete the algorithm

--- Partial Breadth first search algorithm.cpp
+++ Partial Breadth first search algorithm.cpp
@@ -3,14 +3,20 @@ void Graph::BFS(int startingVertex)
   visited = new bool[numberOfVertices];
 
   //Initialize visited pointer with false
+  for (int i = 0; i < numberOfVertices; i++)
+  {
+      visited[i] = false;
+  }
 
   list<int> queue;
 
   visited[startingVertex] = true;
 
   //Add the starting vertex to the queue
+  queue.push_back(startingVertex);
 
   //define iterator of type list<int>
+  std::list<int>::iterator i;
 
   while (!queue.empty()) 
   {
@@ -19,6 +25,7 @@ void Graph::BFS(int startingVertex)
     queue.pop_front();
 
     //For loop definition starting from current vertex to last vertex
+    for (auto i = adjacencyLists[currentVertex].begin(); i != adjacencyLists[currentVertex].end(); ++i)
     {
       int adjacentVertex = *listIterator;
 
